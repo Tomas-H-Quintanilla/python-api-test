bbda44adde3ffddffd650d9ffb536b1d38ce4030
tomas <tomas.hernandez@neteris.com>
2024-05-13 15:36:46 +0200

    Improved the endpoint execution to allow the use of variables obtained during an execution

--- modules/EndpointExecution.py
+++ modules/EndpointExecution.py
@@ -18,6 +18,8 @@ def load_endpoint_data(json_location):
         return data
 
 def is_endpoint_for_execution(endpoint,test_data,endpoint_data):
+    
+
     endpoint_data = test_data["endpoints"][endpoint]
     response=True
 
@@ -28,20 +30,49 @@ def is_endpoint_for_execution(endpoint,test_data,endpoint_data):
 
     return response
 
-def execute_endpoint(endpoint, test_data):
-    endpoint_data = test_data["endpoints"][endpoint]
-    if not is_endpoint_for_execution(endpoint,test_data,endpoint_data):
-        return
+def process_result(test_data,endpoint_data,response):
+    dataResponse=response.json()
+    test_data["variables"][endpoint_data["result"]["name"]]=dataResponse
+
+    if "keys" not in endpoint_data:
+        for key in endpoint_data["keys"]:
+            test_data["variables"][endpoint_data["result"]["name"]]= test_data["variables"][endpoint_data["result"]["name"]][key]
+
+def request_call(test_data,endpoint_data):
+    times = endpoint_data.get("repeat",1)
+
+    for _ in range(times):
+        client = APIClient(test_data["servers"][endpoint_data['server']], replace_vars(endpoint_data["url"], test_data['variables']), endpoint_data["method"], endpoint_data.get("headers",[]))
+        response=client.check(endpoint_data.get("expected_code",200),endpoint_data.get("expected_text",None),endpoint_data.get('payload',None))
+        if "result" in endpoint_data:
+           process_result(test_data,endpoint_data,response)
+                
+
+def get_payload(endpoint_data,test_data):
     if 'payload_file' in endpoint_data and "payload" not in endpoint_data:
         endpoint_data['payload'] = load_endpoint_data(f'data/{test_data["name"]}/{endpoint_data['payload_file']}')
     
     if "payload" in endpoint_data:
         endpoint_data['payload'] = replace_vars(endpoint_data['payload'], test_data['variables'])
+                
+
+
+def execute_endpoint(endpoint, test_data):
+    endpoint_data = test_data["endpoints"][endpoint]
+    if not is_endpoint_for_execution(endpoint,test_data,endpoint_data):
+        return
+
+    get_payload(endpoint_data,test_data)
     
-    times = endpoint_data.get("repeat",1)
-    for _ in range(times):     
-        client = APIClient(test_data["servers"][endpoint_data['server']], replace_vars(endpoint_data["url"], test_data['variables']), endpoint_data["method"], endpoint_data.get("headers",[]))
-        client.check(endpoint_data.get("expected_code",200),endpoint_data.get("expected_text",None),endpoint_data.get('payload',None))
+    if not "url_file" in endpoint_data:
+        request_call(test_data,endpoint_data)
+    else:
+        urls= load_endpoint_data(f'data/{test_data["name"]}/{endpoint_data['url_file']}')
+        for url in urls:
+            endpoint_data["url"]=url
+            request_call(test_data,endpoint_data)
+         
+       
 
 def execute_endpoints(test_data):
     for endpoint in test_data["endpoints"]:
