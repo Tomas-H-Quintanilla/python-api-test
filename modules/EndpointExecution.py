8cd6ab4a736e7e8df3dba07b6c8548b6d0cd4c20
tomas <tomas.hernandez@neteris.com>
2024-05-16 16:38:09 +0200

    Moved variable related functions  from the endpoint execution to the variables treating files.

--- modules/EndpointExecution.py
+++ modules/EndpointExecution.py
@@ -1,45 +1,12 @@
 import json
 from .APITester import APIClient
 import yaml
-
-VARIABLE_SEPARATOR="@"
-
-
-def replace_from_keys(content,variable,variables):
-    variable_text=f'${variable}{VARIABLE_SEPARATOR}'+content.split(f'${variable}{VARIABLE_SEPARATOR}')[1].split('$')[0]
-    keys=variable_text[1:].split(f'{VARIABLE_SEPARATOR}')
-    value=variables[keys[0]]["value"]
-    keys.pop(0)
-    for key in keys:
-        value=value[key]
-    
-    return content.replace(f'{variable_text}$', str(value))
-        
+from .VariableTreating import replace_vars,load_file_data
 
 
 
-def replace_vars(content, variables):
-    isJson = False
-    if not isinstance(content, str):
-        content = json.dumps(content)
-        isJson = True
 
-    for variable, value in variables.items():
-        if variable in content:
-            content = content.replace(f'${variable}$', str(value['value']))
-        if f'${variable}{VARIABLE_SEPARATOR}' in content:
-            content=replace_from_keys(content,variable,variables)
 
-        
-    return json.loads(content) if isJson else content
-
-def load_endpoint_data(location,variables):
-    with open(location, 'r') as file:
-        if '.json' in location:
-            return json.loads(replace_vars(file.read(),variables))
-        elif '.yaml' in location:
-            return yaml.safe_load(replace_vars(file.read(),variables))
-        return "Invalid format"
 
 
 
@@ -78,7 +45,7 @@ def request_call(test_data,endpoint_data):
 
 def get_payload(endpoint_data,test_data):
     if 'payload_file' in endpoint_data and "payload" not in endpoint_data:
-        endpoint_data['payload'] = load_endpoint_data(f'data/{test_data["name"]}/{endpoint_data['payload_file']}',test_data['variables'])
+        endpoint_data['payload'] = replace_vars(load_file_data(f'{test_data["name"]}/data/{endpoint_data['payload_file']}'),test_data['variables'])
     
     elif "payload" in endpoint_data:
         endpoint_data['payload'] = replace_vars(endpoint_data['payload'], test_data['variables'])
@@ -95,7 +62,7 @@ def execute_endpoint(endpoint, test_data):
     if not "url_file" in endpoint_data:
         request_call(test_data,endpoint_data)
     else:
-        urls= load_endpoint_data(f'data/{test_data["name"]}/{endpoint_data['url_file']}',test_data['variables'])
+        urls= replace_vars(load_file_data(f'{test_data["name"]}/data/{endpoint_data['url_file']}'),test_data['variables'])
         for url in urls:
             endpoint_data["url"]=url
             request_call(test_data,endpoint_data)
--- modules/VariableTreating.py
+++ modules/VariableTreating.py
@@ -1,6 +1,45 @@
 import json
-from .EndpointExecution import replace_vars
 import yaml
+
+
+VARIABLE_SEPARATOR="@"
+
+
+def replace_from_keys(content,variable,variables):
+    variable_text=f'${variable}{VARIABLE_SEPARATOR}'+content.split(f'${variable}{VARIABLE_SEPARATOR}')[1].split('$')[0]
+    keys=variable_text[1:].split(f'{VARIABLE_SEPARATOR}')
+    value=variables[keys[0]]["value"]
+    keys.pop(0)
+    for key in keys:
+        value=value[key]
+    
+    return content.replace(f'{variable_text}$', str(value))
+        
+
+def replace_vars(content, variables):
+    isJson = False
+    if not isinstance(content, str):
+        content = json.dumps(content)
+        isJson = True
+
+    for variable, value in variables.items():
+        if variable in content:
+            content = content.replace(f'${variable}$', str(value['value']))
+        if f'${variable}{VARIABLE_SEPARATOR}' in content:
+            content=replace_from_keys(content,variable,variables)
+
+        
+    return json.loads(content) if isJson else content
+
+
+def load_file_data(location):
+    with open(location, 'r') as file:
+        if '.json' in location:
+            return json.loads(file.read())
+        elif '.yaml' in location:
+            return yaml.safe_load(file.read())
+        return {}
+
 
 def increment_test_variables(test_data,save=False):
     for variable in test_data["variables"]:
@@ -17,28 +56,19 @@ def replace_within_vars(test_data):
         
 def load_workflow_file(test_data):
     if "workflow_file" in test_data and "workflow" not in test_data:
-        file_location= f'workflows/{test_data["name"]}/{test_data['workflow_file']}'
-        with open(file_location, 'r') as file:
-            if '.json' in file_location:
-                test_data['workflow']= json.loads(file.read())
-            elif '.yaml' in file_location:
-                test_data['workflow']= yaml.safe_load(file.read())
+        test_data['workflow']=load_file_data(f'{test_data["name"]}/workflows/{test_data['workflow_file']}')
             
 def load_variables_from_file(test_data,file_name):
-    file_location=f'variables/{test_data["name"]}/{file_name}'
+    file_location=f'{test_data["name"]}/variables/{file_name}'
     if 'variables' not in test_data:
         test_data['variables'] = {}
 
-    with open(file_location, 'r') as file:
-        data={}
-        if '.json' in file_location:
-            data= json.loads(file.read())
-        elif '.yaml' in file_location:
-            data= yaml.safe_load(file.read())
-        for key, value in data.items():
-            if key not in test_data['variables']:
-                test_data['variables'][key] = value
-                test_data['variables'][key]['file']=file_name
+    data= load_file_data(file_location)
+
+    for key, value in data.items():
+        if key not in test_data['variables']:
+            test_data['variables'][key] = value
+            test_data['variables'][key]['file']=file_name
 
 def load_variables_from_files(test_data):
     if 'variables_files' in test_data:
@@ -46,16 +76,15 @@ def load_variables_from_files(test_data):
             load_variables_from_file(test_data,file_name)
 
 def save_variables_in_file(data_save,file_name):
-    file_location=f'variables/{data_save["name"]}/{file_name}'
+    file_location=f'{data_save["name"]}/variables/{file_name}'
     variables_save={}
+
+    for variable_name in data_save['variables']:
+        if 'file' not in data_save['variables'][variable_name] and file_name!=data_save['variables'][variable_name]['file']:
+            continue
+        variables_save[variable_name]=data_save['variables'][variable_name]
+
     with open(file_location, 'w') as file:
-
-        for variable_name in data_save['variables']:
-
-            if 'file' not in data_save['variables'][variable_name] and file_name!=data_save['variables'][variable_name]['file']:
-                continue
-            variables_save[variable_name]=data_save['variables'][variable_name]
-
         if '.json' in file_location:
             json.dump(variables_save,file)
         elif '.yaml' in file_location:
