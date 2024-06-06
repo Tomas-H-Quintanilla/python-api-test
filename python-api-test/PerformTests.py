0ab6f9222bc9e0452a49058206f3acf25ea19d72
tomas <tomas.hernandez@neteris.com>
2024-06-06 16:49:54 +0200

    Preparing for pip

--- python-api-test/PerformTests.py
+++ python-api-test/PerformTests.py
@@ -1,16 +1,12 @@
 import yaml
-import json
 import argparse
-from modules.VariableTreating import increment_test_variables,replace_within_vars,load_workflow_file,load_variables_from_files,load_servers_file,load_endpoints_files
-from modules.VariableTreating import save_variables_in_files,load_file_data
+from modules.VariableTreating import increment_test_variables, replace_within_vars, load_workflow_file, load_variables_from_files, load_servers_file, load_endpoints_files
+from modules.VariableTreating import save_variables_in_files, load_file_data
 from modules.EndpointExecution import execute_endpoints
 import copy
-from modules.utils import pprint
 import os
 
-
-
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser(
         description='Process the file for the test execution.')
 
@@ -23,24 +19,21 @@ if __name__ == "__main__":
         print('No configuration file specified.')
         exit(500)
 
-    test_data=load_file_data(args.filename)
-    test_data['name']=os.path.dirname(args.filename)
+    test_data = load_file_data(args.filename)
+    test_data['name'] = os.path.dirname(args.filename)
 
     if 'variables' not in test_data:
-            test_data['variables'] = {}
+        test_data['variables'] = {}
 
-    data_save=None
+    data_save = None
 
     load_variables_from_files(test_data)
     increment_test_variables(test_data)
-    data_save=(copy.deepcopy(test_data))
+    data_save = (copy.deepcopy(test_data))
 
-    
     load_servers_file(test_data)
     load_endpoints_files(test_data)
 
-
-   
     replace_within_vars(test_data)
     load_workflow_file(test_data)
 
@@ -49,9 +42,11 @@ if __name__ == "__main__":
     except Exception as e:
         print(e)
     finally:
-
         if 'variables_files' not in test_data:
             with open(args.filename, "w") as yaml_file:
                 yaml.dump(data_save, yaml_file, sort_keys=False)
         elif data_save is not None:
             save_variables_in_files(data_save)
+
+if __name__ == "__main__":
+    main()
